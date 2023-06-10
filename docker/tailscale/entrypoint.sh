909f4441f6b098759a587a31df89b510b8fc1e63
Andrey Gubarev <andrey@andreygubarev.com>
2023-06-10 21:59:04 +0400

    use prebuild docker container

--- docker/tailscale/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -n "${TS_PORTFORWARD}" ]; then
-    TS_LISTEN_PORT=${TS_PORTFORWARD%%:*}
-    TS_REMOTE_ADDR=${TS_PORTFORWARD#*:}
-    nohup socat "TCP-LISTEN:${TS_LISTEN_PORT},fork,reuseaddr" "TCP:${TS_REMOTE_ADDR}" &
-fi
-
-containerboot "$@"
